bdcdf93089cf0c25d39b3254ec21fec74623f64f
VasanthkumarVelusamy <vasanthkumar.work@gmail.com>
2023-08-04 11:12:54 +0530

    Initial Commit

---
+++ UserInterfaceTest/UserInterfaceTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserInterfaceTestApp.swift
+//  UserInterfaceTest
+//
+//  Created by Vasanth Kumar on 04/08/23.
+//
+
+import SwiftUI
+
+@main
+struct UserInterfaceTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
